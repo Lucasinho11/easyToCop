29c53be2f74afe1e3537cc1ff84255ebc2d3fe66
Lucas <lucas.lubasinski11@gmail.com>
2021-05-16 22:00:59 +0200

    nav fixed position :lock:

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-nav">
+<nav class="bg-nav fixed">
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="relative flex items-center justify-between h-16">
       <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
