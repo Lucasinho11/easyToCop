3e5e84a20fa40573d3834f6150452469bcbcdbf4
Lucas <lucas.lubasinski11@gmail.com>
2021-06-03 11:24:26 +0200

    sub page and responsive :iphone:

--- resources/views/sub.blade.php
+++ resources/views/sub.blade.php
@@ -1,5 +1,12 @@
 @extends('layouts.default')
 @section('content')
 @include('partials.nav')
-
+<div class="all-sub">
+    <h1>Abonnement premium</h1>
+    <h2>5,99€/mois</h2>
+    <p>Vous aurez accès à quelques drops</p>
+    <a href="" class="button-sub">
+        Payer
+    </a>
+</div>
 @include('partials.footer')
