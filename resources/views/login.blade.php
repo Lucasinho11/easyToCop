3eb24c9c7042de02b64d570ffa3cb47729232412
Lucas <lucas.lubasinski11@gmail.com>
2021-06-03 16:29:50 +0200

    register/lgoin form :bust_in_silhouette:

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,6 +1,27 @@
 @extends('layouts.default')
 @section('content')
 @include('partials.nav')
-
+<div class="all-login">
+    <h1>Se connecter</h1>
+    <form action="">
+    <div class="mb-3 pt-0">
+        <input type="email" placeholder="email" class="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border border-blueGray-300 outline-none focus:outline-none focus:ring w-full"/>
+    </div>
+    <div class="relative flex w-full flex-wrap items-stretch mb-3">
+        <span class="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300 absolute bg-transparent rounded text-base items-center justify-center w-8 pl-3 py-3">
+            <i class="fas fa-lock"></i>
+        </span>
+        <input type="password" name="login-password" placeholder="Mot de passe " class="px-3 py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white bg-white rounded text-sm border border-blueGray-300 outline-none focus:outline-none focus:ring w-full pl-10"/>
+        
+    </div>
+    <a href="#" class="button-login">
+        <button type="submit">
+            Se connecter
+        </button>
+    </a>
+       
+    
+    </form>
+</div>
 
 @include('partials.footer')
