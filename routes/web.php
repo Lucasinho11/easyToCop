2519fef48d45af4d2c9451f557e239d39af1ed01
Lucas <lucas.lubasinski11@gmail.com>
2021-06-23 13:23:05 +0200

    route index

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', 'App\Http\Controllers\IndexController@index');
 Route::get('/drops', 'App\Http\Controllers\DropsController@index');
 Route::get('/drops/{id}', 'App\Http\Controllers\DropsController@drop');
