92cf21422068e891b1711b99f4052727082a6516
Lucas <lucas.lubasinski11@gmail.com>
2021-06-26 23:32:27 +0200

    width

--- resources/views/actu.blade.php
+++ resources/views/actu.blade.php
@@ -4,7 +4,7 @@
 <div class="actu-container">
     @foreach ($actu as $ac)
         <h1>{{$ac->name}}</h1>
-        <img class="img-actu-actu" src="{{$ac->img}}" alt="">
+        <img class="img-actu-actu" src="{{$ac->img}}" alt="" style="max-height: 60%; max-width:60%">
         <p>{{$ac->description}}</p>
     @endforeach
 </div>
